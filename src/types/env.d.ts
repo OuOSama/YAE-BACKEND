26b66d87289984c30a984b2fb21585c9ad306919
OuOSama <real.ouosama@gmail.com>
2025-12-05 12:06:14 +0700

    🏡 chore : update dependencies

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,11 @@
 declare module 'bun' {
 	interface Env {
 		PORT: string
+		VLLM_LLM_KEY: string
+		VLLM_LLM_URL: string
+		VLLM_EMB_KEY: string
+		VLLM_EMB_URL: string
+
+		NODE_ENV: 'test' | 'production'
 	}
 }
