17232f205de054982011a7e068144e84757fce0f
OuOSama <real.ouosama@gmail.com>
2026-01-02 16:30:51 +0700

    🗑️ chore (index.ts) remove test route

--- src/modules/ai/index.ts
+++ src/modules/ai/index.ts
@@ -1,15 +1,10 @@
 // src/modules/ai/index.ts
 
 import Elysia from 'elysia'
-import { requireScope } from 'src/plugins/requireScope'
 import { AiModel } from './model'
 import { AiService } from './service'
 
 export const ai = new Elysia({ prefix: '/ai' })
-	.use(requireScope(['ai']))
-	.get('/test-scope', () => {
-		return { auth: 'Hello!' }
-	})
 	.post(
 		'/chat',
 		async ({ body }) => {
