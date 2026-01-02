4fa0792d5652fa72f5b2e07f250d351d60ce0f53
OuOSama <real.ouosama@gmail.com>
2026-01-02 16:27:48 +0700

    ✨ feat (schema) : add schema and setup drizzle client

---
+++ src/database/schema.ts
@@ -0,0 +1,10 @@
+// src/database/schema.ts
+
+// 📖 App Service
+export * from './schema/app_services'
+
+// 🛡️ Authentication
+export * from './schema/auth/accounts'
+export * from './schema/auth/sessions'
+export * from './schema/auth/users'
+export * from './schema/auth/verifications'
