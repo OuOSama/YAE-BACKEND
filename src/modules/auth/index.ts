61cedc2d6b5e86b49dac7bc91143c51992d17610
OuOSama <real.ouosama@gmail.com>
2026-01-08 19:48:02 +0700

    🧙🏻 chore(auth) : rename auth to authRoute

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -4,7 +4,7 @@ import cors from '@elysiajs/cors'
 import Elysia from 'elysia'
 import { serviceAuth, userAuth } from 'src/lib/auth'
 
-export const auth = new Elysia()
+export const authRoute = new Elysia()
 	// frontend
 	.use(cors({ origin: ['http://localhost:3000'], credentials: true }))
 	// user auth
