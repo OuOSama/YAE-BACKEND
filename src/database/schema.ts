c6c26155ad742a3b38619e2910012ed5dcaaa74b
OuOSama <real.ouosama@gmail.com>
2026-06-21 18:41:49 +0700

    📦 database : add secret_keys schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -5,6 +5,7 @@ export * from './schema/app_services'
 
 // 🛡️ Authentication
 export * from './schema/auth/accounts'
+export * from './schema/auth/secret_keys'
 export * from './schema/auth/sessions'
 export * from './schema/auth/users'
 export * from './schema/auth/verifications'
