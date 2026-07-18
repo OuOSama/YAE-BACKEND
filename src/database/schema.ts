bb1e16a1185a8d5712d499ea1152e7e7facd90f9
OuOSama <real.ouosama@gmail.com>
2026-07-18 22:50:31 +0700

    📦 feat ( database ) : add sessions, users, verifications, accounts, table

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,9 +1,21 @@
+export * from './schema/auth/accounts';
+export * from './schema/auth/sessions';
+export * from './schema/auth/users';
+export * from './schema/auth/verifications';
 export * from './schema/test_users';
 
+import { accounts } from './schema/auth/accounts';
+import { sessions } from './schema/auth/sessions';
+import { users } from './schema/auth/users';
+import { verifications } from './schema/auth/verifications';
 import { test_users } from './schema/test_users';
 
 export const table = {
 	test_users,
+	sessions,
+	users,
+	verifications,
+	accounts,
 } as const;
 
 export type Table = typeof table;
