aa9d0bd57b32d023dddca92c73499b68e802e7bd
OuOSama <real.ouosama@gmail.com>
2026-07-18 20:42:34 +0700

    🚀 feat ( route ) : add cors, oauth

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import { Elysia } from 'elysia';
 import { userManage } from '@/modules/users';
+import { auth } from './lib/auth';
+import { yaeCors } from './plugins/cors';
 import { openApiPlugin } from './plugins/openapi';
 
 const app = new Elysia()
 	.use(openApiPlugin)
+	.use(yaeCors)
 	.use(userManage)
 	.get('/', () => 'Hello Elysia')
+	.mount(auth.handler)
 	.listen(process.env.PORT ?? 3000);
 
 console.log(
