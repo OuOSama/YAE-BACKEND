c06a53a7cc579478bf5704f8e45475f9ac393740
OuOSama <real.ouosama@gmail.com>
2026-07-18 22:53:48 +0700

    ⚙️ chore ( auth.ts ) : add usePlural & trustedOrigins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,16 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/database/client'; // your drizzle instance
 
+import { table } from '@/database/schema';
+
 export const auth = betterAuth({
+	baseURL: process.env.BACKEND_URL,
 	database: drizzleAdapter(db, {
 		provider: 'pg', // or "mysql", "sqlite"
+		schema: table,
+		usePlural: true,
 	}),
+	trustedOrigins: [process.env.FRONTEND_URL],
 	socialProviders: {
 		discord: {
 			clientId: process.env.DISCORD_CLIENT_ID,
