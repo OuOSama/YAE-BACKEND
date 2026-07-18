ce437ada0566576c7b0d27215c1b8c0ddb120d23
OuOSama <real.ouosama@gmail.com>
2026-07-18 21:41:01 +0700

    ⚙️ chore ( cors ) : add FRONTEND_URL

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -3,7 +3,7 @@ import { Elysia } from 'elysia';
 
 export const yaeCorsPlugin = new Elysia().use(
 	cors({
-		origin: process.env.BACKEND_URL,
+		origin: process.env.FRONTEND_URL,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
