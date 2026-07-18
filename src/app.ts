cf652dec2950f467e4d86171e536e7c63d682dfc
OuOSama <real.ouosama@gmail.com>
2026-07-18 18:17:29 +0700

    🚀 refactor ( app.ts ) : add "http://"

--- src/app.ts
+++ src/app.ts
@@ -7,5 +7,5 @@ const app = new Elysia()
 	.listen(process.env.PORT ?? 3000);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
