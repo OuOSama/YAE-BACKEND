fe0fa9d73afd905c0abda02856c976299e5cbbbc
OuOSama <real.ouosama@gmail.com>
2026-07-19 02:04:14 +0700

    🚀 feat ( cors ) : add as global

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -7,5 +7,5 @@ export const yaeCorsPlugin = new Elysia().use(
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
-	}),
+	}).as('global'),
 );
